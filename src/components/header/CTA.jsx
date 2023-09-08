84259b230a64d9b454583e2e7df5f3c7cabbb547
Maria Garcia <mgarcia18@hawk.iit.edu>
2023-09-08 16:37:02 -0500

    updated resume, hid contact btn

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import resume from '../../assets/cv.pdf'
+import resume from '../../assets/MariaGarcia_Resume_2023.pdf'
 
 const CTA = () => {
   return (
     <div className='cta'>
       <a href={resume} className='btn'>Download Resume</a>
-      <a href="#contact" className='btn btn-primary'>Contact Me</a>
+      {/* <a href="#contact" className='btn btn-primary'>Contact Me</a> */}
     </div>
   )
 }
