612a8b3a1dcfc5f5233a57fa13210d6c85ec29e9
Maria Garcia <mgarcia18@hawk.iit.edu>
2023-09-12 15:24:12 -0500

    footer and minor header edits

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import './footer.css'
+import {BsLinkedin, BsCalendar2DateFill} from 'react-icons/bs'
+import {FaGithub} from 'react-icons/fa'
 
 const Footer = () => {
   return (
-    <div id="footer"></div>
+    <footer>
+      <a href="#" className="footer__logo">MARI GARCI</a>
+
+      <ul className="permalinks">
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        {/* <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contacts">contact</a></li> */}
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://www.linkedin.com/in/mariagarcia-cs/" target='_blank'><BsLinkedin/></a>
+        <a href="https://github.com/MariaGarciaCS" target='_blank'><FaGithub/></a>
+        <a href="https://calendly.com/marigarci" target='_blank'><BsCalendar2DateFill/></a>
+      </div>
+
+      <div className="footer__copyright">A website by Maria Garcia</div>
+
+    </footer>
   )
 }
 
