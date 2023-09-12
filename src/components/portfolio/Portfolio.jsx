e1e43f9b7ae7d5bc85fde60d80879f8095e8bfcb
Maria Garcia <mgarcia18@hawk.iit.edu>
2023-09-11 20:22:48 -0500

    Projects outline

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import './portfolio.css'
+import SDS from '../../assets/SDS.jpg'
+import GOTV2023 from '../../assets/GOTV2023.png'
+import MG from '../../assets/MG.png'
 
 const Portfolio = () => {
   return (
@@ -7,6 +10,43 @@ const Portfolio = () => {
       <section id="portfolio">
       <h5>Check Out</h5>
       <h2>My Projects</h2>
+      
+
+      <div className="container portfolio__container">
+
+      <div className="portfolio__item">
+        <div className="portfolio__item-image">
+          <img src={MG}/>
+        </div>
+        <h3>This Website</h3>
+        <div className="portfolio__item-cta">
+          <a href="https://github.com/scarletstudio/transithealth" className='btn' target='_blank'>GitHub</a>
+          <a href="https://scarletstudio.github.io/transithealth/" className='btn btn-primary'target='_blank'>Site</a>
+        </div>
+      </div>
+
+       <div className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={GOTV2023}/>
+          </div>
+          <h3>GOTV 2023</h3>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com/scarletstudio/transithealth" className='btn' target='_blank'>GitHub</a>
+            <a href="https://scarletstudio.github.io/transithealth/" className='btn btn-primary'target='_blank'>Site</a>
+          </div>
+        </div>
+
+        <div className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={SDS}/>
+          </div>
+          <h3>Transit Health</h3>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com/scarletstudio/transithealth" className='btn' target='_blank'>GitHub</a>
+            <a href="https://scarletstudio.github.io/transithealth/" className='btn btn-primary'target='_blank'>Site</a>
+          </div>
+        </div>
+      </div>
       </section>
     </div>
   )
