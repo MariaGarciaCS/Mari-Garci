d403f6a93a56f48d3af2bc104c93e2cc20985397
Maria Garcia <mgarcia18@hawk.iit.edu>
2023-10-01 19:26:32 -0500

    added react-router, leads to home page

--- src/index.js
+++ src/index.js
@@ -2,5 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 
-ReactDOM.render(<App/>, document.querySelector("#root"))
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <App />,
+    },
+  ]);
+
+  ReactDOM.render(<RouterProvider router={router} />, document.querySelector("#root"))
