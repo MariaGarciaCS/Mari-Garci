f99d11c855da6f7f2663b452b0a49a7cdf15ad75
Maria Garcia <mgarcia18@hawk.iit.edu>
2023-11-13 17:35:54 -0600

    updated header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Maria Garcia</h1>
-        <h5 className="text-light">Software Developer and Data Engineer</h5>
+        <h5 className="text-light">Full-Stack Software Developer with a Love for Data</h5>
 
         <CTA />
         <HeaderSocials />
